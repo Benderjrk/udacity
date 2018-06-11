73a5d45741f04239662e416c741cfebf2a18a3b5
Justin Bender <justin.bender@MAC10.local>
2018-06-11 11:59:24 -0400

    feat: Adding alt tags to pictures

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.photograph_alt;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -158,7 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  li.append(more);
 
   return li
 }
