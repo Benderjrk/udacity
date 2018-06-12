f5ad5f4763980f5eee71f910b4394319912f699e
Justin Bender <justin.bender@MAC10.local>
2018-06-11 20:27:47 -0400

    feat: Finishing sw touches and aria              didn't find many aria-roles that I felt needed additions

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
+//credit to https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers for the help on understanding
+
 let version = 'v6-attempt';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(version).then(function(cache) {
+        caches.open(version)
+            .then(function(cache) {
             return cache.addAll([
                 '/img/1.jpg',
                 '/img/2.jpg',
@@ -13,14 +16,17 @@ self.addEventListener('install', function(event) {
                 '/img/7.jpg',
                 '/img/8.jpg',
                 '/img/9.jpg',
-                '/img/9.jpg',
+                '/img/10.jpg',
                 '/css/styles.css',
                 '/css/screen1340styles.css',
+                '/css/screen760styles.css',
                 '/data/restaurants.json',
                 '/js/main.js',
                 '/js/dbhelper.js',
                 '/css',
-                '/'
+                '/',
+                '/index.html',
+                'restaurant.html'
 
             ]);
         })
@@ -28,12 +34,13 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('activate', function(event) {
-    var cacheWhitelist = ['v6-attempt'];
+    var whitelist = ['v6-attempt'];
     console.log('activate');
     event.waitUntil(
-        caches.keys().then(function(keyList) {
+        caches.keys()
+            .then(function(keyList) {
             return Promise.all(keyList.map(function(key) {
-                if (cacheWhitelist.indexOf(key) === -1) {
+                if (whitelist.indexOf(key) === -1) {
                     return caches.delete(key);
                 }
             }));
@@ -41,18 +48,22 @@ self.addEventListener('activate', function(event) {
     );
 });
 
+//copies pages it has been on before...but not the ones it hasn't
 self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-        // caches.match() always resolves
-        // but in case of success response will have value
-        if (response !== undefined) {
-            return response;
-        } else {
-            return fetch(event.request).then(function (response) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+            return response || fetch(event.request)
+                .then(function(response) {
+                let responseClone = response.clone();
+                caches.open(version)
+                    .then(function(cache) {
+                    cache.put(event.request, responseClone);
+                });
                 return response;
-            }).catch(function () {
-                return caches.match('/');
             });
-        }
-    }));
+        }).catch(function(err) {
+            return console.log(err);
+        })
+    );
 });
